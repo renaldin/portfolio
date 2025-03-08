9ea431d4a07d5b097d8f1ccd19ee5cc0b29ffbf1
renaldin1 <renaldinoviandi0@gmail.com>
2025-03-09 04:42:04 +0700

    add package

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  distDir: "out",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: "./",
   reactStrictMode: true,
   swcMinify: true,
   images: {
