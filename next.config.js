491529af75592bafd71cf444434eaf367baa92dd
renaldin1 <renaldinoviandi0@gmail.com>
2025-03-09 05:25:41 +0700

    Adjust config

--- next.config.js
+++ next.config.js
@@ -2,13 +2,21 @@
 const nextConfig = {
   output: "export",
   distDir: "out",
-  assetPrefix: "./",
-  reactStrictMode: true,
-  swcMinify: true,
   images: {
-    domains: ['renaldin.github.io'],
     unoptimized: true,
   },
+  assetPrefix: "./",
 }
+// const nextConfig = {
+//   output: "export",
+//   distDir: "out",
+//   assetPrefix: "./",
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   images: {
+//     domains: ['renaldin.github.io'],
+//     unoptimized: true,
+//   },
+// }
 
 module.exports = nextConfig
