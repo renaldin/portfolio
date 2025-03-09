9b5118bc26c33a4448692c97958e87b6556c927b
renaldin1 <renaldinoviandi0@gmail.com>
2025-03-09 08:42:12 +0700

    Updates

---
+++ _next/static/chunks/pages/projects-d78efdf115d3437e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[327],{7670:function(n,e,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/projects",function(){return u(9587)}])},9587:function(n,e,u){"use strict";u.r(e);var t=u(5893);let _=()=>(0,t.jsx)("p",{children:"Heelo"});e.default=_}},function(n){n.O(0,[774,888,179],function(){return n(n.s=7670)}),_N_E=n.O()}]);
